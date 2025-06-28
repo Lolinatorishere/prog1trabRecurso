7dd47368beb799d481592600b59715134c226f1f
Gabriel Banks <78425011+Lolinatorishere@users.noreply.github.com>
2025-06-28 19:25:06 +0100

    Initial commit
    Setup Build Script and tested it with main
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   build/build.c
    	new file:   build/commands.txt
    	new file:   source/main.c

---
+++ build/build.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int64_t size = 0;
+    FILE *fp = fopen("./build/commands.txt", "r");
+    if(fp == NULL) return 1;
+    fseek(fp,0,SEEK_END);
+    size = ftell(fp);
+    fseek(fp,0,SEEK_SET);
+    if(size == 0) return 1;
+    char *commands = malloc(sizeof(char)*size);
+    for(int i = 0 ; i < size ; i++){
+        fseek(fp,i,SEEK_SET);
+        commands[i] = fgetc(fp);
+    }
+    system(commands);
+    free(commands);
+    fclose(fp);
+}
+
+
